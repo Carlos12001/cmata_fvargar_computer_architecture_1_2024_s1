be830e6bf11b01e8decd7e3a6b896d832bb986b3
Carlos12001 <carlos.andres12001@gmail.com>
2024-03-16 22:13:52 -0600

    feat: create binary file

---
+++ src/test/create_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  // Números a escribir en el archivo.
+  unsigned int registers[] = {0x0180, 0x0340};
+
+  // Abrir el archivo en modo de escritura binaria.
+  FILE *file =
+      fopen("input.bin", "wb");  // Nota el uso de "wb" para escritura binaria
+  if (file == NULL) {
+    printf("Error al abrir el archivo.\n");
+    return 1;
+  }
+
+  size_t numElements = sizeof(registers) / sizeof(unsigned int);
+  for (size_t i = 0; i < numElements; ++i) {
+    fwrite(&registers[i], sizeof(unsigned int), 1, file);
+  }
+
+  // Cerrar el archivo.
+  fclose(file);
+
+  printf("Archivo 'input.bin' creado exitosamente.\n");
+
+  return 0;
+}
