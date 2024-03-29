da58d9d3f5e8f9a0fb68b41a61348c483a965af2
Carlos12001 <carlos.andres12001@gmail.com>
2024-03-28 20:07:01 -0600

    feat: add more bits float points

--- src/wav_to_q15_16.py
+++ src/wav_to_q15_16.py
@@ -1,24 +1,29 @@
 import wave
 import numpy as np
 
-def wav_to_q15_16(k,alpha,wav_filename,bin_filename="input.bin"):
+def wav_to_q(k,alpha,wav_filename,bin_filename="input.bin",q=30):
     """
     Convert WAV to binary format, with additional parameters k and alpha.
     
     Parameters:
     k (int): An unsigned integer parameter.
-    alpha (float): A parameter in Q15.16 format.
+    alpha (float): A parameter in Q format.
     wav_filename (str): Name of the input WAV file.
     bin_filename (str): Name of the output binary file.
+    q (int): Number of bits for the fractional part of the output.
     """
     if not isinstance(k, int):
         raise TypeError("k must be an integer")
     if not isinstance(alpha, float):
         raise TypeError("alpha must be a float")
+    if not isinstance(q, int):
+        raise TypeError("q must be an integer")
     if k < 0:
         raise ValueError("k must be non-negative")
     if alpha < 0 or 1 < alpha:
         raise ValueError("alpha must be in the range [0, 1]")
+    if 7 < q and q < 30:
+        raise ValueError("q must be in the range [7, 30]")
 
     with wave.open(wav_filename, "rb") as wav_file:
         # Read the parameters
@@ -42,18 +47,18 @@ def wav_to_q15_16(k,alpha,wav_filename,bin_filename="input.bin"):
         # Normalize the samples to range [-1, 1]
         normalized_samples = audio_samples / 2**15
         
-        # Convert to Q15.16
-        q15_16_samples = np.int32(normalized_samples * 2**16)
+        # Convert to Q floating point
+        q_samples = np.int32(normalized_samples * 2**q)
         
-        # Prepare k and alpha for writing; k as uint32, alpha as Q15.16
+        # Prepare k and alpha for writing; k as uint32, alpha as Q format
         k_uint32 = np.array([k], dtype=np.uint32)
-        alpha_int32 = np.int32(alpha * 2**16)
+        alpha_int32 = np.int32(alpha * 2**q)
         
         # Write to binary file
         with open(bin_filename, "wb") as bin_file:
             k_uint32.tofile(bin_file)  # Write k
             bin_file.write(alpha_int32.tobytes())  # Write alpha
-            q15_16_samples.tofile(bin_file)  # Write audio data
+            q_samples.tofile(bin_file)  # Write audio data
 
 if __name__ == "__main__":
     import os
@@ -70,7 +75,7 @@ if __name__ == "__main__":
     
     
     try:
-        wav_to_q15_16(k,alpha,wavfile,output)
+        wav_to_q(k,alpha,wavfile,output)
 
         print(( f"The file {wavfile} was successfully converted to {output}"
                 f"with k={k} and alpha={alpha}."))
